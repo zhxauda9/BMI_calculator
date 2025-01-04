0c8da48c7784ead8623378298b54cdcbe1eaffb2
azhalgas <zhalgasovaida@gmail.com>
2025-01-04 22:59:44 +0500

    finished

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
-    res.status(404).send(`Web page not found.`)
 });
 
 app.post('/calculate-bmi', (req, res) => {
     const weight = parseFloat(req.body.weight);
     const height = parseFloat(req.body.height);
+    const age=parseFloat(req.body.age);
+    const gender=req.body.gender;
+    const ethnicity=req.body.ethnicity;
 
     if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
         return res.send('Invalid input. Please enter positive numbers for weight and height.');
@@ -26,19 +28,44 @@ app.post('/calculate-bmi', (req, res) => {
 
     if (age > 60) {
         bmi += 1;
-      }
+    }
+    
     if (gender === 'female') {
         bmi -= 1;
     }
-    if (bmi < 18.5) {
-        category = 'Underweight';
-    } else if (bmi < 24.9) {
-        category = 'Normal weight';
-    } else if (bmi < 29.9) {
-        category = 'Overweight';
-    } else {
-        category = 'Obese';
+
+    if(ethnicity=='asian'){
+        if(bmi<18.5){
+            category='Underweight';
+        }else if(bmi<23.0){
+            category='Normal weight';
+        }else if(bmi<27.0){
+            category='Overweight';
+        }else{
+            category='Obese';
+        }
+    }else if(ethnicity=='european'){
+        if(bmi<18.5){
+            category='Underweight';
+        }else if(bmi<25.0){
+            category='Normal weight';
+        }else if(bmi<30.0){
+            category='Overweight';
+        }else{
+            category='Obese';
+        }
+    }else if(ethnicity=='african'){
+        if(bmi<19){
+            category='Underweight';
+        }else if(bmi<26.0){
+            category='Normal weight';
+        }else if(bmi<32.0){
+            category='Overweight';
+        }else{
+            category='Obese';
+        }
     }
+
     res.render('result', {
         bmi: bmi.toFixed(2),
         category: category
